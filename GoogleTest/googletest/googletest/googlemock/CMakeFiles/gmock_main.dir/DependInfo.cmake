0bc97de1d80039a5d699fa3890f13e5c56e2dccc
Your Name <you@example.com>
2017-03-29 11:36:04 +0200

    initial commit

---
+++ GoogleTest/googletest/googletest/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jenkins/cginternals/cmake-init/source/tests/googletest/googletest/src/gtest-all.cc" "/home/jenkins/cginternals/cmake-init/source/tests/googletest/googlemock/CMakeFiles/gmock_main.dir/__/googletest/src/gtest-all.cc.o"
+  "/home/jenkins/cginternals/cmake-init/source/tests/googletest/googlemock/src/gmock-all.cc" "/home/jenkins/cginternals/cmake-init/source/tests/googletest/googlemock/CMakeFiles/gmock_main.dir/src/gmock-all.cc.o"
+  "/home/jenkins/cginternals/cmake-init/source/tests/googletest/googlemock/src/gmock_main.cc" "/home/jenkins/cginternals/cmake-init/source/tests/googletest/googlemock/CMakeFiles/gmock_main.dir/src/gmock_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "source/tests/googletest/googlemock/include"
+  "source/tests/googletest/googlemock"
+  "source/tests/googletest/googletest/include"
+  "source/tests/googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
